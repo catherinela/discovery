9b91f2b848aff0f6fa938e25d108138f5748ee83
邵芳怡 <shaofangyi@meituan.com>
2022-03-08 11:18:45 +0800

    feature/搭建框架

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FC } from 'react';
+import Layout from './components/layout';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+const App: FC = () => (
+    <Layout/>
+)
 
 export default App;
---
+++ src/components/layout/index.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Layout, Menu } from 'antd';
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+} from '@ant-design/icons';
+import './index.css';
+
+const { Header, Sider, Content } = Layout;
+
+interface menuParams {
+    name: string;
+    enthusiasmLevel?: number;
+  }
+
+export default class LayoutDemo extends React.Component {
+  state = {
+    collapsed: false,
+  };
+
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  };
+  handleClick = (e: any) => {
+    console.log('click ', e);
+  };
+
+  render() {
+    return (
+      <Layout style={{height:"100vh"}}>
+        <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} onClick={this.handleClick}>
+            <Menu.Item key="1" icon={<UserOutlined />}>
+              nav 1
+            </Menu.Item>
+            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+              nav 2
+            </Menu.Item>
+            <Menu.Item key="3" icon={<UploadOutlined />}>
+              nav 3
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }}>
+            {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+              className: 'trigger',
+              onClick: this.toggle,
+            })}
+          </Header>
+          <Content
+            className="site-layout-background"
+            style={{
+              margin: '24px 16px',
+              padding: 24,
+              minHeight: 280,
+            }}
+          >
+            Content
+          </Content>
+        </Layout>
+      </Layout>
+    );
+  }
+}
